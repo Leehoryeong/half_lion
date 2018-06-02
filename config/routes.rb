f4d1f19b03678aadb55b879cf5e0785fe821e3fb
HoryeongLee <horyeong0326@likelion.org>
2018-06-02 04:42:24 +0000

    basic form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   resources :musics
   root 'musics#index'
   # The priority is based upon order of creation: first created -> highest priority.
