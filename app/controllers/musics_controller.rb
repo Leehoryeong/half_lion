eb4683a90a759e5ebae1212ec8d942c0f2e55078
HoryeongLee <horyeong0326@likelion.org>
2018-06-02 02:45:38 +0000

    controller

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,2 +1,23 @@
 class MusicsController < ApplicationController
+  
+  def index
+  end
+  
+  def new
+  end
+    
+  def create
+  end
+    
+  def edit
+  end
+  
+  def update
+  end
+  
+  private
+  def music_params
+end
+  
+    
 end
